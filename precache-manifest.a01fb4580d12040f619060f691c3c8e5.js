5f9946659cfa84fbfec9da8178b29204d9b54103
Ronaldo <bueno120@hotmail.com>
2020-05-04 14:16:47 -0300

    Primeiro commit

---
+++ precache-manifest.a01fb4580d12040f619060f691c3c8e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7e9ad7320262b5e261f3fe03606d64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff7bac9a8100346fc2d",
+    "url": "/static/css/main.4f023909.chunk.css"
+  },
+  {
+    "revision": "2c226f2a49957b3d8f33",
+    "url": "/static/js/2.ceb02515.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ceb02515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff7bac9a8100346fc2d",
+    "url": "/static/js/main.85141dbb.chunk.js"
+  },
+  {
+    "revision": "ab82c91d607c6ebdbb60",
+    "url": "/static/js/runtime-main.62dd15b1.js"
+  },
+  {
+    "revision": "dc6b130f2e65eba5385c20858954b66d",
+    "url": "/static/media/imghome.dc6b130f.png"
+  }
+]);
