8dfb78de9b42f4cd5a5c7328d06fccefaf51ec64
ronaldobrs <55709651+ronaldobrs@users.noreply.github.com>
2020-05-04 22:03:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import './pg1.css'
+import { Link } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Pg1(){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        return(
+            
+            <div className='main'>
+
+                <p className='titulo'>Solicitação de Inclusão no Serviço Pré-Hospitalar de Urgência e 
+                Emergência</p>
+                
+                <img src={require('./linhatempo1.png')} className='linhatempo1'/>
+                <div className='divtexto1'>
+
+                    <h2>Confirme seus dados para prosseguir</h2>
+                    <p>Pelo presente, eu <input className='inputpg1'/>, titular do plano de saúde individual/familiar da
+                    Unimed Nordeste RS contrato nº <input className='inputpg1'/>, solicito a minha inclusão e dos meus dependentes, 
+                    a partir do primeiro dia do mês subsequente, no Serviço Pré-Hospitalar de Urgência e 
+                    Emergência, oferecido pela Unimed Nordeste RS a seus beneficiários, conforme detalhes a 
+                    seguir.</p>
+
+                </div>
+
+                <div className='boxpreco'>
+
+                    <p className='texto1'>Produtos Contratados</p>
+                    <p className='texto2'>SOS Unimed - Pessoa Física</p>   
+                    <h2 className='texto3'>R$ 00,00/mês</h2>
+                    <p className='texto4'>Condição comercial: xxxxxxxxxxx</p>
+                    <p className='texto4'>Percentual de desconto: 00%</p>
+                    <p className='texto4'>Período: 00 meses</p>
+                    <p className='texto4'>Valor Unitário: 00,00</p>
+                    <p className='texto4'>Início da vigência: 01/04/2020</p>
+
+                </div>
+
+                <p className='btn1'>Alterar Dados</p>
+                <Link to='/components/pg2'><p className='btn2'>Confirmar</p></Link>
+                
+            </div>
+        )
+    }
+
+export default Pg1
